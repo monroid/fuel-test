a46afef81db754fc34e1faca7e3edaea9df6f6e6
Dmitry Ilyin <idv1985@gmail.com>
2013-06-29 02:15:28 +0400

    Documentation refactoring

--- doc/latex.sh
+++ doc/latex.sh
@@ -5,3 +5,11 @@ make latex
 cd "_build/latex" || exit 1
 make
 mv "FuelTestingSuit.pdf" "../.."
+cd "../.."
+if [ "`uname`" = "Darwin" ]; then
+  open "FuelTestingSuit.pdf"
+elif [ "`uname`" = "Linux" ]; then
+  xdg-open "FuelTestingSuit.pdf"
+else
+  exit 1
+fi
---
+++ fake_devops/helpers/helpers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+Devops fake helpers
+"""
+
+
+def _get_file_size(self):
+    pass
+
+
+def _wait(self):
+    pass
+
+
+def ssh(self):
+    pass
