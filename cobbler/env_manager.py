fb053e242ddd4be70ed2a73765beb16dbf8e7be7
monroid <a.urlapova@gmail.com>
2013-06-28 13:32:42 +0400

    merge with DI

---
+++ cobbler/env_manager.py
@@ -0,0 +1,87 @@
+from time import sleep
+from root import root
+import tarfile
+
+from devops.manager import Manager
+
+class EnvManager():
+    login = "root"
+    password = "r00tme"
+
+    def __init__(self, base_image='/var/lib/libvirt/images/ubuntu-12.04.1-server-amd64-base.qcow2'):
+        self.manager = Manager()
+        self.environment = self.manager.environment_create('test_env')
+        self.base_image = base_image
+        self._create_env()
+
+    def _create_env(self):
+        internal = self.manager.network_create(environment=self.environment, name='internal', pool=None)
+        external = self.manager.network_create(environment=self.environment, name='external', pool=None)
+        private = self.manager.network_create(environment=self.environment, name='private', pool=None)
+
+        node = self.manager.node_create(name='node', environment=self.environment)
+
+        self.manager.interface_create(node=node, network=internal)
+        self.manager.interface_create(node=node, network=external)
+        self.manager.interface_create(node=node, network=private)
+
+        volume = self.manager.volume_get_predefined(self.base_image)
+
+        v3 = self.manager.volume_create_child('test_vol', backing_store=volume, environment=self.environment)
+
+        self.manager.node_attach_volume(node=node, volume=v3)
+
+        self.environment.define()
+
+        self.environment.start()
+
+    def _remote(self, node_name='node', net_name='public'):
+        return self.environment().node_by_name(node_name).remote(net_name, login=self.login, password=self.password)
+
+
+    def create_snapshot_env(self, snap_name="", description="", force=True):
+        self.environment.snapshot(name=snap_name, description=description, force=force)
+
+    def revert_snapshot_env(self, snap_name="", destroy=True):
+        self.environment.revert(name=snap_name, destroy=destroy)
+
+    def erase_env(self):
+        self.environment.erase()
+
+    def execute_cmd(self, command, node_name='node', net_name='public'):
+        self._remote(node_name=node_name, net_name=net_name).execute(command)
+
+    def upload(self, source, dest, node_name='node', net_name='public'):
+        self._remote(node_name=node_name, net_name=net_name).upload(source, dest)
+
+    def upload_modules(self, node_name='node', remote_dir="/etc/puppet/modules/"):
+        module_dir = root('deployment', 'puppet')
+
+        remote = self._remote(node_name=node_name)
+
+        tar_file = None
+        try:
+            tar_file = remote.open('/tmp/recipes.tar', 'wb')
+
+            with tarfile.open(fileobj=tar_file, mode='w', dereference=True) as tar:
+                tar.add(module_dir, arcname='')
+
+            remote.mkdir(remote_dir)
+            remote.check_call('tar xmf /tmp/recipes.tar --overwrite -C %s' % remote_dir)
+        finally:
+            if tar_file:
+                tar_file.close()
+
+
+if __name__ == "__main__":
+    env = EnvManager()
+
+    sleep(600) #TODO: add await method
+
+    env.create_snapshot_env(snap_name="test1")
+
+    env.execute_cmd('apt-get install mc')
+
+    env.erase_env()
+
+
